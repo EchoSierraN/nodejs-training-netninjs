05e24ceae82484f92fd26d6f30c00d13a17350f8
Elias Shemsu <echosierran@gmail.com>
2024-04-19 15:45:58 +0300

    Saving and retrieving values into a document

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 const app = express();
 //connect to mongodb
 const dbURI =
-  "mongodb+srv://echosierran:0ZCzGlq1Na1136Wt@cluster0.nmgsweq.mongodb.net/";
+  "mongodb+srv://echosierran:0ZCzGlq1Na1136Wt@cluster0.nmgsweq.mongodb.net/dbBlogs?retryWrites=true&w=majority&appName=Cluster0";
 mongoose
   .connect(dbURI)
   .then((result) => {
@@ -23,6 +24,37 @@ app.use(express.static("public"));
 app.use(morgan("dev"));
 
 //mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "New blog 2",
+    snippet: "More about the new blog",
+    body: "Even more about the new blog",
+  });
+
+  blog.save().then((result) => {
+    res.send(result);
+  });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("66226383b686871f20d4fbe5")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use((req, res, next) => {
   console.log("========== NEW REQUEST ==============");
